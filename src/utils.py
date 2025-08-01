a205f8a0224ca8165a1702b4356540d538004550
swas2004 <swastikruchira@gmail.com>
2025-08-01 09:18:53 +0530

    data transformation

---
+++ src/utils.py
@@ -0,0 +1,14 @@
+import numpy as np
+import dill
+import pandas as pd
+from src.exception import CustomExceptionHandling
+import os
+
+def save_object(file_path,obj):
+    try:
+        dir_path=os.path.dirname(file_path)
+        os.makedirs(dir_path,exist_ok=True)
+        with open(file_path,"wb") as file_obj:
+            dill.dump(obj,file_obj)
+    except Exception as e:
+        raise CustomExceptionHandling(e,sys)
